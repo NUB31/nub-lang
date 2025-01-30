47b88f58f3eca67f3462b24e6e1d709f5d33e8a3
nub31 <oliverlhs@outlook.com>
2025-01-30 16:34:06 +0100

    Remove debug util

--- output/build.sh
+++ output/build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 nasm -g -felf64 out.asm -o out.o
 nasm -g -felf64 ../input/core/strlen.asm -o strlen.o
-nasm -g -felf64 ../input/core/util.asm -o util.o
 
-ld -o out strlen.o util.o out.o
+ld -o out strlen.o out.o
